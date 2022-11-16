52b1e59e5046a279c232dec5196bc554617117d8
YaelShlomo <86185261+YaelShlomo@users.noreply.github.com>
2022-11-16 09:32:44 +0200

    async await

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from "@angular/platform-browser"
 import { AppComponent } from "./app.component";
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
-import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { TaskDetailsFormTDComponent } from './task-details-form-td/task-details-form-td.component';
 import { TaskDetailsFromMDComponent } from './task-details-from-md/task-details-from-md.component';
 import { TaskService } from "./task.service";
+import { AsyncAwaitDemoComponent } from "./async-await-demo/async-await-demo.component";
 
 @NgModule({
-    declarations: [AppComponent, TaskListComponent, TaskDetailsComponent, TaskDetailsFormTDComponent, TaskDetailsFromMDComponent],
-    imports: [BrowserModule, FormsModule, ReactiveFormsModule ],
+    declarations: [AppComponent, TaskListComponent, TaskDetailsComponent, TaskDetailsFormTDComponent,
+        TaskDetailsFromMDComponent, AsyncAwaitDemoComponent],
+    imports: [BrowserModule, FormsModule, ReactiveFormsModule],
     providers: [TaskService],
     bootstrap: [AppComponent],
 
